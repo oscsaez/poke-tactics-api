2f317fc1c4606426c2bef7e590a9b4ee35a9a9d2
oscsaez <oscar.sg.dnh@gmail.com>
2025-06-28 12:55:49 +0200

    ✨ Add uri constants

--- PokeTactics.Api/Utils/ApiConstants.cs
+++ PokeTactics.Api/Utils/ApiConstants.cs
@@ -5,5 +5,9 @@ namespace PokeTactics.Api.Utils
         public const string ContentType = "application/json";
         public const string DefaultConnection = "DefaultConnection";
         public const string MigrationsAssembly = "PokeTactics.Infrastructure";
+
+        // HostedService
+        public const string BasePokemonInfoUri = "https://pokeapi.co/api/v2/";
+        public static readonly string AllPokemonInfoUri = $"{BasePokemonInfoUri}pokemon/?limit={int.MaxValue}";
     }
 }
