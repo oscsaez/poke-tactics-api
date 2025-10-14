c50a1fe247b6719ac0908942143467ef92c8f506
oscsaez <oscar.sg.dnh@gmail.com>
2025-10-15 00:57:44 +0200

    💡 Add TODO

--- PokeTactics.Core/Utils/CoreConstants.cs
+++ PokeTactics.Core/Utils/CoreConstants.cs
@@ -8,6 +8,8 @@ namespace PokeTactics.Core.Utils
         public const int MaxAbilitiesListCount = 3;
         public const int MaxPokemonNameLength = 30;
         public const int MaxAbilityNameLength = 20;
+
+        // TODO Check if less length for move name is ok
         public const int MaxMoveNameLength = 100;
         public const int MaxTypeNameLength = 8;
         public const int MaxSpriteUriLength = 150;
