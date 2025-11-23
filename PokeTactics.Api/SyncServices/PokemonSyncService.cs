d9370f72ee29b848e5c91ee70d8ed24683f38992
oscsaez <oscar.sg.dnh@gmail.com>
2025-11-23 13:58:41 +0100

    ♻ Refactor PokemonSyncService

--- PokeTactics.Api/SyncServices/PokemonSyncService.cs
+++ PokeTactics.Api/SyncServices/PokemonSyncService.cs
@@ -16,14 +16,12 @@ public class PokemonSyncService : IPokemonSyncService
     private readonly IUnitOfWork _unitOfWork;
     private readonly ILogger<AbilitySyncService> _logger;
     private readonly HttpClient _httpClient;
-    private readonly SemaphoreSlim _semaphore;
 
     public PokemonSyncService(IUnitOfWork unitOfWork, ILogger<AbilitySyncService> logger, IHttpClientFactory httpClientFactory)
     {
         _unitOfWork = unitOfWork;
         _logger = logger;
         _httpClient = httpClientFactory.CreateClient(ApiConstants.ExternalApiName);
-        _semaphore = new SemaphoreSlim(ApiConstants.NumberOfRequestsGrantedConcurrently);
     }
 
     public async Task Sync(CancellationToken cancellationToken)
@@ -39,145 +37,138 @@ public class PokemonSyncService : IPokemonSyncService
         ConcurrentBag<Pokemon> newPokemonConcurrentList = [];
         ConcurrentBag<Pokemon> updatedPokemonConcurrentList = [];
         ConcurrentBag<PokemonPokeApiResponse> newPokemonConcurrentResponses = [];
-        List<Task> apiCalls = [];
 
-        foreach (PokemonSummaryPokeApiResponse pokemonSummary in allPokemonResponse.Results)
+        ParallelOptions parallelOptions = new ParallelOptions 
+        { 
+            MaxDegreeOfParallelism = ApiConstants.NumberOfRequestsGrantedConcurrently, 
+            CancellationToken = cancellationToken 
+        };
+
+        await Parallel.ForEachAsync(allPokemonResponse.Results, parallelOptions, async (pokemonSummary, ct) =>
         {
-            await _semaphore.WaitAsync(cancellationToken);
-
-            apiCalls.Add(Task.Run(async () =>
+            try
             {
-                try
+                var pokemonInfoResponse = await _httpClient.GetFromJsonAsync<PokemonPokeApiResponse>(pokemonSummary.Url, ct)
+                    ?? throw new PokeApiSyncException(MessagesHelper.GetApiCallFailMessage(pokemonSummary.Url));
+
+                if (existingPokemonMap.TryGetValue(pokemonSummary.Name, out Pokemon existingPokemon))
                 {
-                    PokemonPokeApiResponse pokemonInfoResponse =
-                        await _httpClient.GetFromJsonAsync<PokemonPokeApiResponse>(pokemonSummary.Url, cancellationToken)
-                        ?? throw new PokeApiSyncException(MessagesHelper.GetApiCallFailMessage(pokemonSummary.Url));
+                    var pokemonFromResponse = pokemonInfoResponse.ToPokemonWithAbilitiesAndMoves();
 
-                    if (existingPokemonMap.TryGetValue(pokemonSummary.Name, out Pokemon existingPokemon))
+                    if (!existingPokemon.Compare(pokemonFromResponse))
                     {
-                        Pokemon pokemonFromResponse = pokemonInfoResponse.ToPokemonWithAbilitiesAndMoves();
-
-                        if (existingPokemon.Compare(pokemonFromResponse))
-                        {
-                            return;
-                        }
-
                         existingPokemon.MapExisting(pokemonFromResponse, existingAbilitiesMap, existingMovesMap);
                         updatedPokemonConcurrentList.Add(existingPokemon);
                     }
-                    else
-                    {
-                        Pokemon pokemonFromResponse = pokemonInfoResponse.ToPokemon();
-
-                        newPokemonConcurrentList.Add(pokemonFromResponse);
-                        newPokemonConcurrentResponses.Add(pokemonInfoResponse);
-                    }
                 }
-                catch (Exception ex)
+                else
                 {
-                    _logger.LogError(ex, "Error while trying to get new pokemon or the ones to be updated.");
+                    newPokemonConcurrentList.Add(pokemonInfoResponse.ToPokemon());
+                    newPokemonConcurrentResponses.Add(pokemonInfoResponse);
                 }
-                finally
-                {
-                    _semaphore.Release();
-                }
-            }, cancellationToken));
-        }
-
-        await Task.WhenAll(apiCalls);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error processing pokemon {}", pokemonSummary.Name);
+            }
+        });
 
         await _unitOfWork.PokemonDao.CreateRangeAsync(newPokemonConcurrentList.ToList());
         await _unitOfWork.PokemonDao.UpdateRangeAsync(updatedPokemonConcurrentList.ToList());
-        await _unitOfWork.CommitAsync();
 
         if(!newPokemonConcurrentList.IsNullOrEmpty())
         {
             List<PokemonPokeApiResponse> newPokemonResponses = newPokemonConcurrentResponses.ToList();
 
-            await CreateAbilitiesInPokemon(newPokemonResponses);
-            await CreateMovesInPokemon(newPokemonResponses);
+            await CreateAbilitiesInPokemon(newPokemonResponses, existingPokemonMap, existingAbilitiesMap);
+            await CreateMovesInPokemon(newPokemonResponses, existingPokemonMap, existingMovesMap);
         }
+
+        await _unitOfWork.CommitAsync();
 
         _logger.LogInformation("Creation/Update of pokemon has been done!");
     }
 
-    private async Task CreateAbilitiesInPokemon(ICollection<PokemonPokeApiResponse> pokemonResponses)
+    private async Task CreateAbilitiesInPokemon(
+        ICollection<PokemonPokeApiResponse> pokemonResponses,
+        IDictionary<string, Pokemon> existingPokemonMap,
+        IDictionary<string, Ability> existingAbilitiesMap)
     {
-        IEnumerable<KeyValuePair<string, AbilitySlotPokeApiResponse>> pokemonAndAbilityNamesPairs = 
-            from p in pokemonResponses
-            from a in p.Abilities
-            select new KeyValuePair<string, AbilitySlotPokeApiResponse>(p.Name, a);
-
-        IDictionary<string, Pokemon> existingPokemonMap = await _unitOfWork.PokemonDao.LoadMapByName();
-        IDictionary<string, Ability> existingAbilitiesMap = await  _unitOfWork.AbilityDao.LoadMapByName();
+        IEnumerable<KeyValuePair<string, AbilitySlotPokeApiResponse>> pokemonAndAbilityNamesPairs = pokemonResponses
+            .SelectMany(p => p.Abilities.Select(a => new KeyValuePair<string, AbilitySlotPokeApiResponse>(p.Name, a)));
         
         List<AbilityInPokemon> newAbilitiesInPokemon = [];
+        HashSet<AbilityInPokemonKey> abilitiesInPokemonSeen = [];
+
         foreach (KeyValuePair<string, AbilitySlotPokeApiResponse> pokemonAndAbilityNamesPair in pokemonAndAbilityNamesPairs)
         {
-            if (existingPokemonMap.TryGetValue(pokemonAndAbilityNamesPair.Key, out Pokemon pokemon))
+            if (!existingPokemonMap.TryGetValue(pokemonAndAbilityNamesPair.Key, out Pokemon pokemon))
             {
-                if (existingAbilitiesMap.TryGetValue(pokemonAndAbilityNamesPair.Value.AbilityInfo.Name, out Ability ability))
-                {
-                    if (!newAbilitiesInPokemon.Any(x => IsEqual(x, ability.Id, pokemon.Id, pokemonAndAbilityNamesPair.Value.IsHidden)))
-                    {
-                        newAbilitiesInPokemon.Add(new AbilityInPokemon
-                        {
-                            AbilityId = ability.Id,
-                            PokemonId = pokemon.Id,
-                            IsHidden = pokemonAndAbilityNamesPair.Value.IsHidden
-                        });
-                    }
-                }
+                continue;
             }
+
+            if (!existingAbilitiesMap.TryGetValue(pokemonAndAbilityNamesPair.Value.AbilityInfo.Name, out Ability ability))
+            {
+                continue;
+            }
+
+            AbilityInPokemonKey key = new(pokemon.Id, ability.Id, pokemonAndAbilityNamesPair.Value.IsHidden);
+
+            if (!abilitiesInPokemonSeen.Add(key))
+            {
+                continue;
+            }
+
+            newAbilitiesInPokemon.Add(new AbilityInPokemon
+            {
+                AbilityId = ability.Id,
+                PokemonId = pokemon.Id,
+                IsHidden = pokemonAndAbilityNamesPair.Value.IsHidden
+            });
         }
 
         await _unitOfWork.AbilityInPokemonDao.CreateRangeAsync(newAbilitiesInPokemon);
-        await _unitOfWork.CommitAsync();
     }
 
-    private async Task CreateMovesInPokemon(ICollection<PokemonPokeApiResponse> pokemonResponses)
+    private async Task CreateMovesInPokemon(
+        ICollection<PokemonPokeApiResponse> pokemonResponses,
+        IDictionary<string, Pokemon> existingPokemonMap,
+        IDictionary<string, Move> existingMovesMap)
     {
-        IEnumerable<KeyValuePair<string, string>> pokemonAndMoveNamesPairs = 
-            from p in pokemonResponses
-            from m in p.Moves
-            select new KeyValuePair<string, string>(p.Name, m.MoveUriPokeApiResponse.Name);
-
-        IDictionary<string, Pokemon> existingPokemonMap = await _unitOfWork.PokemonDao.LoadMapByName();
-        IDictionary<string, Move> existingMovesMap = await  _unitOfWork.MoveDao.LoadMapByName();
+        IEnumerable<KeyValuePair<string, string>> pokemonAndMoveNamesPairs = pokemonResponses
+            .SelectMany(p => p.Moves.Select(m => new KeyValuePair<string, string>(p.Name, m.MoveUriPokeApiResponse.Name)));
         
         List<MoveInPokemon> newMovesInPokemon = [];
+        HashSet<MoveInPokemonKey> movesInPokemonSeen = []; 
         foreach (KeyValuePair<string, string> pokemonAndMoveNamesPair in pokemonAndMoveNamesPairs)
         {
-            if (existingPokemonMap.TryGetValue(pokemonAndMoveNamesPair.Key, out Pokemon pokemon))
+            if (!existingPokemonMap.TryGetValue(pokemonAndMoveNamesPair.Key, out Pokemon pokemon))
             {
-                if (existingMovesMap.TryGetValue(pokemonAndMoveNamesPair.Value, out Move move))
-                {
-                    if (!newMovesInPokemon.Any(x => IsEqual(x, move.Id, pokemon.Id)))
-                    {
-                        newMovesInPokemon.Add(new MoveInPokemon
-                        {
-                            MoveId = move.Id,
-                            PokemonId = pokemon.Id
-                        });
-                    }
-                }
+                continue;
             }
+
+            if (!existingMovesMap.TryGetValue(pokemonAndMoveNamesPair.Value, out Move move))
+            {
+                continue;
+            }
+
+            MoveInPokemonKey key = new(pokemon.Id, move.Id);
+
+            if (!movesInPokemonSeen.Add(key))
+            {
+                continue;
+            }
+
+            newMovesInPokemon.Add(new MoveInPokemon
+            {
+                MoveId = move.Id,
+                PokemonId = pokemon.Id
+            });
         }
 
         await _unitOfWork.MoveInPokemonDao.CreateRangeAsync(newMovesInPokemon);
-        await _unitOfWork.CommitAsync();
     }
 
-    private static bool IsEqual(AbilityInPokemon abilityInPokemon, int abilityId, int pokemonId, bool isHidden)
-    {
-        return abilityInPokemon.AbilityId == abilityId &&
-            abilityInPokemon.PokemonId == pokemonId &&
-            abilityInPokemon.IsHidden == isHidden;
-    }
-
-    private static bool IsEqual(MoveInPokemon moveInPokemon, int moveId, int pokemonId)
-    {
-        return moveInPokemon.MoveId == moveId &&
-            moveInPokemon.PokemonId == pokemonId;
-    }
+    private readonly record struct AbilityInPokemonKey(int PokemonId, int AbilityId, bool IsHidden);
+    private readonly record struct MoveInPokemonKey(int PokemonId, int MoveId);
 }
