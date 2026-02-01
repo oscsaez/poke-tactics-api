33d41f8830adba5979ffd6dee63c2d60375eef68
oscsaez <oscar.sg.dnh@gmail.com>
2026-02-01 10:58:16 +0100

    ♻️ Refactor test

--- PokeTactics.Test/Core/Utils/Extensions/StringExtensionsTest.cs
+++ PokeTactics.Test/Core/Utils/Extensions/StringExtensionsTest.cs
@@ -20,39 +20,18 @@ public class StringExtensionsTest
     }
 
     [Theory]
-    [InlineData(false, false)]
-    [InlineData(false, true)]
-    [InlineData(true, true)]
-    public void CompareStringCollections(bool areEqual, bool haveSameLength)
+    [InlineData(new[] { "a", "b" }, new[] { "a", "b" }, true)]
+    [InlineData(new[] { "a", "b" }, new[] { "b", "a" }, true)]
+    [InlineData(new[] { "a", "b" }, new[] { "a" }, false)]
+    [InlineData(new[] { "a", "b" }, new[] { "a", "c" }, false)]
+    [InlineData(new string[] { }, new string[] { }, true)]
+    [InlineData(new[] { "a", "a" }, new[] { "a", "b" }, false)]
+    public void CompareStringCollections(string[] collection1, string[] collection2, bool expected)
     {
-        // Arrange
-        ICollection<string> collection1 = ["a", "b"];
-        ICollection<string> collection2 = ["a", "b"];
-
-        if (!areEqual)
-        {
-            if (haveSameLength)
-            {
-                collection2.Remove("b");
-                collection2.Add("c");
-            }
-            else
-            {
-                collection2.Add("b");
-            }
-        }
-
         // Act
         bool result = collection1.Compare(collection2);
 
         // Assert
-        if (areEqual)
-        {
-            Assert.True(result);
-        }
-        else
-        {
-            Assert.False(result);
-        }
+        Assert.Equal(expected, result);
     }
 }
